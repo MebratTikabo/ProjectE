926aa4124fe5696a5c34a029dc2d7ef059089da7
Habteab <Habteab@habteab>
2024-01-03 21:25:20 -0500

    initial commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,10 @@
+package e;
+
+public class DemoE {
+public static void main(String[] args) {
+	 System.out.println("git practice E");
+	 
+	 System.out.println("The repository is created");
+	 System.out.println("it is going good");
+}
+}
